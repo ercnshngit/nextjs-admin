8e06addaf8dabe526b29806449c0ee4fc12b9550
Ercan Şahin <ercansahin@outlook.com>
2023-12-04 14:13:53 +0300

    Add react-simple-code-editor component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,10 @@
+import JSONEditInput from "@/components/block-builder/components/json-edit-input";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className="w-1/2 h-48">
+      <JSONEditInput />
+    </div>
+  );
 }
--- src/components/block-builder/components/json-edit-input.tsx
+++ src/components/block-builder/components/json-edit-input.tsx
@@ -1,20 +1,47 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-json";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/libs/utils";
+import { highlight, languages } from "prismjs";
+import Editor from "react-simple-code-editor";
 
 export default function JSONEditInput() {
-  const [code, setCode] = useState([]);
-  const [mode, setMode] = useState<"code" | "ui">("code");
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+  const [code, setCode] = useState('[{"dsfsdfds":"sdfsdfsdf"}]');
+  const [mode, setMode] = useState<"edit" | "code" | "ui">("code");
+
+  const parseCode = (code: string) => {
+    try {
+      return JSON.stringify(JSON.parse(code), null, 4);
+    } catch (e) {
+      console.log("hata", e);
+      return code;
+    }
+  };
+
+  const parsedCode = parseCode(code);
   return (
-    <div className="w-full relative">
-      <pre>
-        <code className={"language-json"}>{code}</code>
-      </pre>
+    <div className="w-full h-full flex flex-col gap-4 mt-4">
+      <div className="flex items-center gap-2 self-end">
+        <Switch
+          id="ui-mode-switcher"
+          checked={mode === "ui"}
+          onCheckedChange={(checked) => setMode(checked ? "ui" : "code")}
+        />
+        <Label htmlFor="ui-mode-switcher">UI</Label>
+      </div>
+      {mode === "ui" && <div></div>}
+      {mode === "code" && (
+        <Editor
+          value={parsedCode}
+          onValueChange={(code) => setCode(code)}
+          highlight={(code) => highlight(code, languages.json, "json")}
+          padding={10}
+          className="w-full h-full rounded-md text-[#D4D4D4] bg-[#1E1E1E] "
+        />
+      )}
     </div>
   );
 }
