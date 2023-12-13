69ba7d6ef12823c9375bff93e0daea8a53c27505
Mete Çorbacı <metecorbaci22@hotmail.com>
2023-12-13 16:59:35 +0300

    Add column-relation create route and update column-relation dto and table service

--- src/app/api/column-relation/create/route.ts
+++ src/app/api/column-relation/create/route.ts
@@ -2,13 +2,11 @@ import { TableService } from "@/services/table.service";
 
 export async function POST(
     request: Request,
-    { params }: { params: { column_id: number } }
   ) {
-    const column_id = params.column_id
     const tableService = new TableService()
     const body = await request.json()
     try {
-        return await tableService.createCrudOption(column_id , body);
+        return await tableService.createColumnRelation(body);
     } catch (error) {
         return new Response(JSON.stringify({status : "error" , message : error}));        
     }
--- src/services/dto/column-relation.dto.ts
+++ src/services/dto/column-relation.dto.ts
@@ -1,9 +1,9 @@
 interface ColumnRelationCreateDto {
     table_id: number;
-    reference_table_id: number;
+    referenced_table_id: number;
     pivot_table_id: number;
     column_id: number;
-    reference_column_id: number;
+    referenced_column_id: number;
     relation_type_id: number;
     foreign_key_name: string;
 }
