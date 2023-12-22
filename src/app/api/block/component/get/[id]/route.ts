a2fc5e61f72df3b1c4be4b1cd95be126e44a03aa
Ercan Şahin <ercansahin@outlook.com>
2023-12-22 21:52:21 +0300

    Refactor API route code

--- src/app/api/block/component/get/[id]/route.ts
+++ src/app/api/block/component/get/[id]/route.ts
@@ -1,46 +1,62 @@
-import { BlockComponentService } from "@/services/block_component.service"
-import { LogService } from "@/services/log.service"
-import cors from "@/utils/cors"
-import { NextRequest } from "next/server"
+import { BlockComponentService } from "@/services/block_component.service";
+import { LogService } from "@/services/log.service";
+import cors from "@/utils/cors";
+import { NextRequest } from "next/server";
 
 export async function GET(
-    req: NextRequest,
-    { params }: { params: { id: number } }
+  req: NextRequest,
+  { params }: { params: { id: number } }
 ) {
-    const blockComponentService = new BlockComponentService()
-    try {
-        const res = await blockComponentService.getBlockComponent(Number(params.id))
-        return cors(req, res);
-    } catch (error) {
-        await blockComponentService.createLog({ error }, req.nextUrl.pathname);
-        console.log(error)
-        const res = new Response(JSON.stringify({ status: "error", message: error }),{status: 500}); 
-        return cors(req, res);
-    }
+  const blockComponentService = new BlockComponentService();
+  try {
+    const res = await blockComponentService.getBlockComponent(
+      Number(params.id)
+    );
+    if (!res)
+      return new Response(
+        JSON.stringify({ status: "error", message: "Not found" }),
+        { status: 404 }
+      );
+    return cors(req, res);
+  } catch (error) {
+    await blockComponentService.createLog({ error }, req.nextUrl.pathname);
+    console.log(error);
+    const res = new Response(
+      JSON.stringify({ status: "error", message: error }),
+      { status: 500 }
+    );
+    return cors(req, res);
+  }
 }
 
 export async function POST(
-    req: NextRequest,
-    { params }: { params: { id: number } }
+  req: NextRequest,
+  { params }: { params: { id: number } }
 ) {
-    const blockComponentService = new BlockComponentService()
-    try {
-        const body = await req.json()
-        const res = await blockComponentService.updateBlockComponent(Number(params.id), body)
-        return cors(req, res);
-    } catch (error) {
-        await blockComponentService.createLog({ error }, req.nextUrl.pathname);
-        console.log(error)
-        const res = new Response(JSON.stringify({ status: "error", message: error }),{status: 500}); 
-        return cors(req, res);
-    }
+  const blockComponentService = new BlockComponentService();
+  try {
+    const body = await req.json();
+    const res = await blockComponentService.updateBlockComponent(
+      Number(params.id),
+      body
+    );
+    return cors(req, res);
+  } catch (error) {
+    await blockComponentService.createLog({ error }, req.nextUrl.pathname);
+    console.log(error);
+    const res = new Response(
+      JSON.stringify({ status: "error", message: error }),
+      { status: 500 }
+    );
+    return cors(req, res);
+  }
 }
 
-export async function OPTIONS(request: Request) { 
-    return cors(
-      request,
-      new Response(null, {
-        status: 204,
-      })
-    );
-  }
+export async function OPTIONS(request: Request) {
+  return cors(
+    request,
+    new Response(null, {
+      status: 204,
+    })
+  );
+}
